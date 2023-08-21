2b23e4ac2b0033cb7f8f2e82f9e36b0db3c7cf8a
nghiahoang02 <hducnghia@gmail.com>
2023-08-21 15:16:42 +0700

    make category page

---
+++ aht/src/components/PaginatorCategory.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="paginator">
+        <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
+        <span>{{ currentPage }} / {{ totalPages }}</span>
+        <button @click="nextPage" :disabled="currentPage === totalPages">Next</button>
+    </div>
+</template>
+  
+<script>
+    export default {
+        name: "PaginatorCategory",
+        props: {
+            currentPage: Number,
+            totalPages: Number,
+        },
+        methods: {
+            prevPage() {
+                this.$emit("page-change", this.currentPage - 1);
+            },
+            nextPage() {
+                this.$emit("page-change", this.currentPage + 1);
+            },
+        },
+    };
+</script>
+  
--- aht/src/views/CategoryPage.vue
+++ aht/src/views/CategoryPage.vue
@@ -15,20 +15,36 @@
 		<div class="filtered-box">
 			<h2 class="heading">Filtered by</h2>
 			<!-- add filter here -->
-			<p class="clear-button">Clear all</p>
+			<div v-if="selectedCategory" class="filtered-category">
+				<p>Types: {{ selectedCategory }}</p>
+				<button class="remove-filter" @click="unselectCategory">X</button>
+			</div>
+			<div v-if="selectedBrand" class="filtered-category">
+				<p>Brand: {{ selectedBrand }}</p>
+				<button class="remove-filter" @click="unselectBrand">X</button>
+			</div>
+			<p class="clear-button" @click="unselectAll">Clear all</p>
 		</div>
 		<div class="category-box">
 			<div class="types">
 				<h2 class="heading">Types</h2>
 				<!-- add types here -->
-			</div>
-			<div class="features">
-				<h2 class="heading">Features</h2>
-				<!-- add features here -->
+				<div class="dropdown">
+					<button class="dropdown-button" @click="toggleDropdown">Select a category</button>
+					<div class="dropdown-content" v-show="showDropdown">
+						<p v-for="(category, index) in uniqueCategories" :key="index" @click="selectCategory(category)">{{ category }}</p>
+					</div>
+				</div>
 			</div>
 			<div class="brands">
 				<h2 class="heading">Brands</h2>
 				<!-- add brands here -->
+				<div class="dropdown">
+					<button class="dropdown-button" @click="toggleDropdown">Select a brand</button>
+					<div class="dropdown-content" v-show="showDropdown">
+						<p v-for="(brand, index) in uniqueBrand" :key="index" @click="selectBrand(brand)">{{ brand }}</p>
+					</div>
+				</div>
 			</div>
 			<div class="price">
 				<h2 class="heading">Price</h2>
@@ -39,39 +55,23 @@
 	<div class="product-box">
 		<div class="paginator">
 			<!-- page -->
+			<PaginatorCategory
+				:current-page="currentPage"
+				:total-pages="totalPages"
+				@page-change="handlePageChange"
+			/>
 		</div>
 		<div class="product-content">
-			<!-- v-for here for product lists -->
-			<div class="product-container">
-				<img src="https://lh3.googleusercontent.com/a/AAcHTtesfsMyGystqOyV9PLVfB9wNQtxTTfan--M5PnZ9Y53vSQ=s288-c-no">
-				<p class="product-name">ProductName</p>
-				<p class="product-price">100$</p>
-			</div>
-			<div class="product-container">
-				<img src="https://lh3.googleusercontent.com/a/AAcHTtesfsMyGystqOyV9PLVfB9wNQtxTTfan--M5PnZ9Y53vSQ=s288-c-no">
-				<p class="product-name">ProductName</p>
-				<p class="product-price">100$</p>
-			</div>
-			<div class="product-container">
-				<img src="https://lh3.googleusercontent.com/a/AAcHTtesfsMyGystqOyV9PLVfB9wNQtxTTfan--M5PnZ9Y53vSQ=s288-c-no">
-				<p class="product-name">ProductName</p>
-				<p class="product-price">100$</p>
-			</div>
-			<div class="product-container">
-				<img src="https://lh3.googleusercontent.com/a/AAcHTtesfsMyGystqOyV9PLVfB9wNQtxTTfan--M5PnZ9Y53vSQ=s288-c-no">
-				<p class="product-name">ProductName</p>
-				<p class="product-price">100$</p>
-			</div>
-			<div class="product-container">
-				<img src="https://lh3.googleusercontent.com/a/AAcHTtesfsMyGystqOyV9PLVfB9wNQtxTTfan--M5PnZ9Y53vSQ=s288-c-no">
-				<p class="product-name">ProductName</p>
-				<p class="product-price">100$</p>
+			<div class="product-container" v-for="product in displayedProducts" :key="product.id">
+				<img :src="product.thumbnail" alt="Product Thumbnail">
+				<p class="product-name">{{ product.title }}</p>
+				<p class="product-price">{{ product.price }}$</p>
 			</div>
 		</div>
 	</div>
 </content>
 </template>
-<script>
+<!-- <script>
     export default {
 		name: "CategoryPage",
 		data() {
@@ -121,6 +121,94 @@
 			} 
 		}
     }
+</script> -->
+<script>
+	import PaginatorCategory from "@/components/PaginatorCategory.vue";
+	export default {
+		name: "CategoryPage",
+		components: {
+			PaginatorCategory,
+		},
+		data() {
+			return {
+				products: [],
+				msg: '',
+				itemsPerPage: 8,
+				currentPage: 1,
+				showDropdown: false,
+				selectedCategory: '',
+				selectedBrand: ''
+			};
+		},
+		computed: {
+			totalPages() {
+				return Math.ceil(this.products.length / this.itemsPerPage);
+			},
+			displayedProducts() {
+				const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+				const endIndex = startIndex + this.itemsPerPage;
+				return this.products.slice(startIndex, endIndex);
+			},
+			uniqueCategories() {
+				// Extract unique category names from products array
+				const categoriesSet = new Set();
+				this.products.forEach((product) => {
+					categoriesSet.add(product.category);
+				});
+				return Array.from(categoriesSet);
+			},
+			uniqueBrand() {
+				// Extract unique category names from products array
+				const brandSet = new Set();
+				this.products.forEach((product) => {
+					brandSet.add(product.brand);
+				});
+				return Array.from(brandSet);
+			},
+		},
+		methods: { 
+			async getCategory() { 
+				
+				try { 
+					const res = await fetch('https://dummyjson.com/products')
+					const response = await res.json();
+					this.products = response.products;
+
+				} catch (error) { 
+					this.msg = 'Error! Could not reach the API. ' + error 
+				} 
+			},
+			handlePageChange(newPage) {
+				this.currentPage = newPage;
+			},
+			toggleDropdown() {
+				this.showDropdown = !this.showDropdown;
+			},
+			selectCategory(category) {
+				this.selectedCategory = category;
+				this.showDropdown = false; // Close the dropdown after selecting a category
+				// Perform actions based on the selected category if needed
+			},
+			selectBrand(brand) {
+				this.selectedBrand = brand;
+				this.showDropdown = false;
+			},
+			unselectCategory() {
+				this.selectedCategory = '';
+			},
+			unselectBrand() {
+				this.selectedBrand = '';
+			},
+			unselectAll() {
+				this.unselectCategory();
+				this.unselectBrand();
+			}
+		},
+		created() {
+			// Call getCategory when the component is created
+			this.getCategory();
+		},
+	}
 </script>
 <style scoped>
 content {
@@ -157,9 +245,11 @@ content {
 	width: 56px;
 	color: #000000;
 	font-family: Montserrat;
-	font-size: 14px;
+	font-size: 16px;
 	letter-spacing: 0;
 	line-height: 22px;
+	cursor: pointer;
+	text-decoration: underline;
 }
 
 .category-box {
@@ -172,10 +262,44 @@ content {
 	box-shadow: 2px 2px 10px 0 rgba(0,0,0,0.08);
 }
 
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-button, .remove-filter {
+  padding: 10px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  background-color: #f9f9f9;
+}
+
+/* .dropdown-content {
+  position: absolute;
+  z-index: 1;
+  background-color: white;
+  border: 1px solid #ccc;
+  width: 100%;
+  max-height: 200px;
+  overflow-y: auto;
+  display: none;
+} */
+
+.dropdown-content p {
+  padding: 8px 12px;
+  margin: 0;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.dropdown-content p:hover {
+  background-color: #f0f0f0;
+}
+
 .product-content {
 	display: flex;
 	flex-wrap: wrap;
-	justify-content: space-between;
+	justify-content: left;
 }
 
 .product-container {
