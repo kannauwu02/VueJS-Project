c897b44a6cba89cfd3237cbfd33b797002bc84e2
HoanDV <dvhoan1197@gmail.com>
2023-08-22 13:53:28 +0700

    1st cm

--- aht/src/main.js
+++ aht/src/main.js
@@ -1,5 +1,36 @@
-import { createApp } from 'vue'
+import { createApp, h } from 'vue'
 import App from './App.vue'
-import router from "./router/index";
+import router from './router/index';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
 
-createApp(App).use(router).mount('#app')
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  uri: 'https://magentoapi.merket.io/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+  headers: {
+    'Accept': '*',
+    'Access-Control-Allow-Origin:': '*',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Access-Control-Allow-Methods': '*',
+    'Content-Type': 'application/json',
+  }
+})
+
+const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+})
+
+const app = createApp({
+  render: () => h(App),
+})
+
+app.use(apolloProvider).use(router).mount('#app')
--- aht/src/views/HomePage.vue
+++ aht/src/views/HomePage.vue
@@ -1,5 +1,18 @@
+<script>
+import { gql, useQuery } from '@apollo/client';
+
+const GET_DOGS = gql`
+  query GetDogs {
+    dogs {
+      id
+      breed
+    }
+  }
+`;
+</script>
 <template>
     <p>{{ msg }}</p>
+    <p>{{ world }}</p>
     <div>
         <router-link to="/">HomePage</router-link>
         <router-link to="/CategoryPage">CategoryPage</router-link>
@@ -7,12 +20,30 @@
     </div>
 </template>
 <script>
-    export default {
-      name: "HomePage",
-      data() {
-        return {
-          msg: "Home",
-        };
-      },
+  import gql from 'graphql-tag';
+  export default {
+    name: "HomePage",
+    data() {
+      return {
+        msg: "Home",
+      };
+    },
+    apollo: {
+      world: gql`query {
+        cmsPage(id: 2) {
+          content
+          content_heading
+          identifier
+          meta_description
+          meta_keywords
+          meta_title
+          page_layout
+          relative_url
+          title
+          type
+          url_key
+        }
+      }`
     }
+  }
 </script>
