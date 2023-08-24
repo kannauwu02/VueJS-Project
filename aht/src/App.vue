e63d8fc48bc215e18a8f5a9dc02e9390aa726ff4
HoanDV <dvhoan1197@gmail.com>
2023-08-24 18:02:34 +0700

    homepage

--- aht/src/App.vue
+++ aht/src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <router-view></router-view>
+  <div class="pageSizing">
+    <TheHeader />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+import TheHeader from "@/components/TheHeader.vue";
+
 export default {
   name: 'App',
   components: {
-    
-  }
+    TheHeader,
+  },
 }
 </script>
 
