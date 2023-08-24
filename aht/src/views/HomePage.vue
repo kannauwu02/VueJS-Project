8cf988739996e2c9247b8b034b22dfb1359635e5
HoanDV <dvhoan1197@gmail.com>
2023-08-24 17:58:35 +0700

    homepage

--- aht/src/views/HomePage.vue
+++ aht/src/views/HomePage.vue
@@ -1,19 +1,4 @@
-<script setup>
-  import StoreConfig from '@/composables/store-config'
-  import { ref, watch } from 'vue'
-
-  const storeData = ref(StoreConfig)
-  console.log(storeData);
-
-  watch(storeData, (newStoreData) => {
-    console.log(newStoreData)
-  })
-
-</script>
-
 <template>
-  
-  <p>{{ config.base_static_url }}</p>
   <div>
     <router-link to="/">HomePage</router-link>
     <router-link to="/CategoryPage">CategoryPage</router-link>
@@ -35,23 +20,8 @@
     },
     apollo: {
       home: gql`query {
-        home: cmsPage(id: 2) {
+        home: cmsPage(identifier: "home") {
           content
-          content_heading
-          identifier
-          meta_description
-          meta_keywords
-          meta_title
-          page_layout
-          relative_url
-          title
-          type
-          url_key
-        },
-      }`,
-      config: gql`query {
-        config: storeConfig {
-          base_static_url
         },
       }`,
     },
